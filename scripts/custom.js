9491970689e3c8ac9f5d884d90f60d68c2c03901
Tony Kosturos <kosturosanthony@gmail.com>
2017-09-28 09:43:58 -0700

    working nav bar

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-   $('#navbar-button').on('hover', function() {
-      $('#navbar-icon').css("background-image", "url('data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0, 0, 0, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E')");
-      });
 
 /*
    $("#li-header-foldable").on({
@@ -23,14 +20,14 @@ $(function() {
 });
 
 */
-
+/*
    $(document).on('mouseenter', "#li-header-foldable", function() {
        $(".subnav").css('visibility', 'visible');
    }).on('mouseleave', function() {
        $(".subnav").css('visibility', 'invisible');
    });
 
-
+*/
 
 });
 
