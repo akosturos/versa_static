1f80ee80614e98805dbc2f22ee48de0d689c73f1
Tony Kosturos <kosturosanthony@gmail.com>
2017-10-04 18:58:53 -0700

    fixed team page

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,34 +1,40 @@
 $(document).ready(function(){
+   x = document.URL
+   //if ( x == ("growversa.com") ) {
 
-/*
-   $("#li-header-foldable").on({
-    mouseenter: function () {
-        $(".subnav").css('visibility', 'visible');
-    },
-    mouseleave: function () {
-        $(".subnav").css('visibility', 'invisible');
-    }
+      $(window).on('scroll', function () {
+
+         if ($(this).scrollTop() > 500) {
+            $('#appear-nav').fadeIn(200);
+         } else {
+            $('#appear-nav').fadeOut(40);
+         }
+
+         if ($(window).scrollTop() > 500)  {
+            $('#appear-home').css('color','#b0eb00');
+         }
+
+         if ($(window).scrollTop() > 1450) {
+            $('#appear-tech').css('color','#b0eb00');
+            $('#appear-home').css('color','#ffffff');
+         } else {
+            $('#appear-tech').css('color','#ffffff');
+         }
+
+      });
+
+      $('#appear-tech').on('click', function() {
+         $('#appear-nav').fadeIn(100);
+         $(this).css('color','#b0eb00');
+      });
+      $('#appear-home').on('click', function() {
+         $('#appear-nav').fadeOut(100);
+      });
+
+   //}
+
+   if ( x == ("coeusenergy.com/team") ) {
+      $('#appear-team').css('color','#b0eb00');
+   }
+
 });
-
-$(function() {
-   $('#li-header-foldable').hover( function(){
-      $(".subnav").css('visibility', 'visible');
-   },
-   function(){
-      $(".subnav").css('visibility', 'invisible');
-   });
-});
-
-*/
-/*
-   $(document).on('mouseenter', "#li-header-foldable", function() {
-       $(".subnav").css('visibility', 'visible');
-   }).on('mouseleave', function() {
-       $(".subnav").css('visibility', 'invisible');
-   });
-
-*/
-
-});
-
-
